01af7b02eeadf0723faa0f4f653014002197d220
tanczost <tanczost23@gmail.com>
2022-04-11 18:38:29 +0200

    remove unused row

--- app/Controllers/Http/ChannelUsersController.ts
+++ app/Controllers/Http/ChannelUsersController.ts
@@ -1,5 +1,4 @@
 import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
-import Channel from 'App/Models/Channel'
 import { ChannelUser } from 'App/Models/ChannelUser'
 
 export interface NewInvitation {
